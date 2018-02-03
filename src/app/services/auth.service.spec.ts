2c2daf3b4c89afefb94172faab314dc4bda294b1
Razvan Tache <rtache@tremend.ro>
2018-02-03 19:32:48 +0200

    Update to auth service and tests

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,42 +1,133 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject, getTestBed, async, tick, fakeAsync} from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {FakeBackendService} from './fakebackend/fake-backend.service';
+
 import moment = require('moment');
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('AuthService', () => {
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        HttpClientModule
-      ],
-      providers: [
-        AuthService,
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: FakeBackendService,
-          multi: true
-        }
-      ]
+      imports: [HttpClientTestingModule],
+      providers: [AuthService]
     });
+
+    httpMock = getTestBed().get(HttpTestingController);
   });
 
   beforeEach( () => {
-    let store = {};
+    const store = {};
 
     spyOn(localStorage, 'getItem').and.callFake((key: string): string => store[key] || null);
     spyOn(localStorage, 'setItem').and.callFake((key: string, value: string): string => {
       return store[key] = <string> value;
     });
+    spyOn(localStorage, 'removeItem').and.callFake((key: string) => {
+      delete store[key];
+    });
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', inject([AuthService], (service: AuthService) => {
     expect(service).toBeTruthy();
   }));
 
+  it('should have user token expired', inject([AuthService], (service: AuthService) => {
+    localStorage.setItem(AuthService.EXPIRES_AT_KEY, JSON.stringify(moment().subtract(20, 'second')));
+
+    expect(service.isLoggedIn()).toBeFalsy();
+    expect(service.isLoggedOut()).toBeTruthy();
+  }));
+
   it('should not have a user logged in', inject([AuthService], (service: AuthService) => {
-    // localStorage.setItem('expires_at', JSON.stringify(moment().add(20, 'second')));
-    expect(service.isLoggedIn()).toBe(false);
+    expect(service.isLoggedIn()).toBeFalsy();
+    expect(service.isLoggedOut()).toBeTruthy();
+  }));
+
+  it('should have user logged in', inject([AuthService], (service: AuthService) => {
+    localStorage.setItem(AuthService.EXPIRES_AT_KEY, JSON.stringify(moment().add(20, 'second')));
+
+    expect(service.isLoggedIn()).toBeTruthy();
+    expect(service.isLoggedOut()).toBeFalsy();
+  }));
+
+  it('should register and login a new user', inject([AuthService], (service: AuthService) => {
+    const response = {
+      token: 'my_key',
+      expiresIn: 20
+    };
+
+    service.register({email: 'razvan@r.com', password: 'myPass', lastName: 'last name', firstName: 'first name'})
+      .subscribe((res) => {
+        expect(res).toEqual(response);
+
+        expect(service.isLoggedIn()).toBeTruthy();
+      });
+    const req = httpMock.expectOne(AuthService.REGISTER_URL);
+    expect(req.request.method).toBe('POST');
+
+    req.flush(response);
+  }));
+
+  it('should fail register for existing username', inject([AuthService], (service: AuthService) => {
+    service.register({email: 'razvan@r.com', password: 'myPass', lastName: 'last name', firstName: 'first name'})
+      .subscribe(
+        res => {},
+        error => {
+          expect(error.statusText).toEqual('Email already exists');
+          expect(service.isLoggedIn()).toBeFalsy();
+        });
+    const req = httpMock.expectOne(AuthService.REGISTER_URL);
+    req.error(new ErrorEvent('INVALID_USERNAME'), {status: 400, statusText: 'Email already exists'});
+  }));
+
+  it('should login user', inject([AuthService], (service: AuthService) => {
+    const response = {
+      token: 'my_key',
+      expiresIn: 20
+    };
+
+    service.login({email: 'razvan@a.com', password: 'myPass'})
+      .subscribe(
+        res => {
+          expect(res).toEqual(response);
+
+          expect(service.isLoggedIn()).toBeTruthy();
+        }
+      );
+
+    const req = httpMock.expectOne(AuthService.LOGIN_URL);
+    expect(req.request.method).toBe('POST');
+    req.flush(response);
+  }));
+
+  it('should fail to login user', inject([AuthService], (service: AuthService) => {
+    service.login({email: 'razvan@a.com', password: 'myPass'})
+      .subscribe(
+        res => {},
+        error => {
+          expect(error.statusText).toBe('Invalid username or password');
+
+          expect(service.isLoggedIn()).toBeFalsy();
+        }
+      );
+
+    const req = httpMock.expectOne(AuthService.LOGIN_URL);
+    req.error(new ErrorEvent('INVALID'), {status: 400, statusText: 'Invalid username or password'});
+  }));
+
+  it('should log out the user', inject([AuthService], (service: AuthService) => {
+    localStorage.setItem(AuthService.TOKEN_KEY, 'custom_token');
+    localStorage.setItem(AuthService.EXPIRES_AT_KEY, JSON.stringify(moment().add(20, 'second')));
+
+    service.logout();
+
+    expect(service.isLoggedIn()).toBeFalsy();
+    expect(service.isLoggedOut()).toBeTruthy();
   }));
 });
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,31 +5,38 @@ import {HttpClient} from '@angular/common/http';
 
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/shareReplay';
+import {RegisterRequest} from '../library/auth/register-request';
+import {LoginRequest} from '../library/auth/login-request';
 
 @Injectable()
 export class AuthService {
+  public static EXPIRES_AT_KEY = 'expires_at';
+  public static TOKEN_KEY = 'token';
+
+  public static REGISTER_URL = '/api/users';
+  public static LOGIN_URL = '/api/authenticate';
 
   constructor(private httpClient: HttpClient) { }
 
-  public login(email: string, password: string) {
-    return this.httpClient.post<any>('/api/authenticate', {email: email, password: password})
-      .do(res => this.setTokenData)
+  public login(loginRequestEntity: LoginRequest) {
+    return this.httpClient.post<any>(AuthService.LOGIN_URL, loginRequestEntity)
+      .do(res => this.setTokenData(res))
       .shareReplay();
   }
 
   public logout() {
-    localStorage.removeItem('expires_at');
-    localStorage.removeItem('token');
+    localStorage.removeItem(AuthService.EXPIRES_AT_KEY);
+    localStorage.removeItem(AuthService.TOKEN_KEY);
   }
 
-  public register({email, password, firstName, lastName}) {
-    return this.httpClient.post<any>('/api/users', {email: email, password: password, firstName: firstName, lastName: lastName})
-      .do(res => this.setTokenData)
+  public register(userRequestEntity: RegisterRequest) {
+    return this.httpClient.post<any>(AuthService.REGISTER_URL, userRequestEntity)
+      .do(res => this.setTokenData(res))
       .shareReplay();
   }
 
   public isLoggedIn(): boolean {
-    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    const expiresAt = JSON.parse(localStorage.getItem(AuthService.EXPIRES_AT_KEY));
 
     return moment().isBefore(moment(expiresAt));
   }
@@ -41,7 +48,7 @@ export class AuthService {
   private setTokenData(authResult) {
     const expiresAt = moment().add(authResult.expiresIn, 'second');
 
-    localStorage.setItem('token', authResult.token);
-    localStorage.setItem('expires_at', JSON.stringify(expiresAt));
+    localStorage.setItem(AuthService.TOKEN_KEY, authResult.token);
+    localStorage.setItem(AuthService.EXPIRES_AT_KEY, JSON.stringify(expiresAt));
   }
 }
