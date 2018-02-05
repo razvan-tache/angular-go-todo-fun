366ef1b382ba6a8281fd0db0efb5570cd65b2c5f
Razvan Tache <rtache@tremend.ro>
2018-02-05 08:15:48 +0200

    add tests for logout

--- src/app/components/logout/logout.component.spec.ts
+++ src/app/components/logout/logout.component.spec.ts
@@ -1,10 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import { LogoutComponent } from './logout.component';
 import {AuthService} from '../../modules/core/services/auth/auth.service';
 import {FormsModule} from '@angular/forms';
 import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {Router} from '@angular/router';
 
 describe('LogoutComponent', () => {
   let component: LogoutComponent;
@@ -22,10 +23,23 @@ describe('LogoutComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LogoutComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
+
+  it('should log out the user and redirect it home', inject(
+    [AuthService, Router],
+    (authService: AuthService, router: Router) => {
+      spyOn(authService, 'logout');
+      spyOn(router, 'navigate');
+
+      fixture.detectChanges();
+      expect(authService.logout).toHaveBeenCalled();
+      expect(router.navigate).toHaveBeenCalledWith(['/']);
+    }
+  ));
 });
