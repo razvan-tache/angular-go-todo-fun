f71a73ed935f2b918b2149e8001c53c220911bd1
Razvan Tache <rtache@tremend.ro>
2018-02-07 23:38:44 +0200

    Add comment to continue e2e tests

--- e2e/tests/flows/auth.e2e-spec.ts
+++ e2e/tests/flows/auth.e2e-spec.ts
@@ -1,4 +1,10 @@
-describe('The auth flows are working', () => {
-
-});
+/**
+ * TODO: Test flows
+ * TODO: UnRegister users should not be able to access a page (add this on every test)
+ * TODO: Register and login should not be redirected
+ * TODO: Sign UP -> logout -> Log In
+ */
+// describe('The auth flows are working', () => {
+//
+// });
 
