855ba3e5aa78ffc1060a3d6d6b7327a8df39d7c7
Razvan Tache <rtache@tremend.ro>
2018-03-01 22:27:06 +0200

    Make menu links on logged in users come from the component

--- src/app/components/logged-in-layout/logged-in-layout.component.ts
+++ src/app/components/logged-in-layout/logged-in-layout.component.ts
@@ -7,8 +7,29 @@ import {AppComponent} from '../app/app.component';
   styleUrls: ['./logged-in-layout.component.css']
 })
 export class LoggedInLayoutComponent extends AppComponent implements OnInit {
+  public menuLinks: MenuLink[] = MenuLinksProvider.getMenuLinks();
 
   ngOnInit() {
   }
 
+}
+
+class MenuLinksProvider {
+  public static getMenuLinks(): MenuLink[] {
+    return [
+      { 'title': 'Some link', 'route': '' },
+      { 'title': 'Some link 2', 'route': '' },
+      { 'title': 'Some link 3', 'route': '' },
+      { 'title': 'Some link 4', 'route': '' },
+      { 'title': 'Some link 5', 'route': '' },
+      { 'title': 'Some link 6', 'route': '' },
+      { 'title': 'Some link 7', 'route': '' },
+    ];
+  }
+}
+
+class MenuLink {
+  public title;
+
+  public route;
 }
