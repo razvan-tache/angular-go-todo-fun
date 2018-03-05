4f5cd3f5e38824c34e2eac59d280d263397e49be
Razvan Tache <rtache@tremend.ro>
2018-03-05 23:40:41 +0200

    Add menu links dinamicaly

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular with Go: To Do Fun';
+export class AppComponent implements OnDestroy {
+  public title = 'To Do Fun';
+
+  public mobileQuery: MediaQueryList;
+
+  protected mobileQueryListener: () => void;
+
+  public constructor(protected changeDetectorRef: ChangeDetectorRef, protected media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this.mobileQueryListener);
+  }
 }
